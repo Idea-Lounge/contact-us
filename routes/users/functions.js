8587c6b73459d42acd760238af93297f15a49ed0
Anurag Sahoo <anurag@aitoelabs.com>
2017-11-14 13:41:23 -0800

    docs(CommentingProtocol): Created commenting protocol. Please check readme

--- routes/users/functions.js
+++ routes/users/functions.js
@@ -8,8 +8,10 @@
       const nodemailer = require('nodemailer');
       // Generate test SMTP service account from ethereal.email
       // Only needed if you don't have a real mail account for testing
+      // codeReview(Anurag): createTestAccount is not required since we already have a real mail account.
       nodemailer.createTestAccount((err, account) => {
         // create reusable transporter object using the default SMTP transport
+        // codeReview(Anurag): transporter object needs to be loaded only once in the beginning and not everytime for every request.
         let transporter = nodemailer.createTransport({
           host: 'smtp.gmail.com',
           port: 587,
@@ -35,6 +37,7 @@
         emailBody += 'Email: ' + requestBody.email + '\n';
         emailBody += 'Message: ' + requestBody.message;
         let mailOptions = {
+          // codeReview(Anurag): IdeaLounge mail information to be stored in config and not here.
           from: '"IdeaLounge 👻" <ideallounge.info@gmail.com>', // sender address
           to: freelancers, // list of receivers
           subject: 'Hello', // Subject line
@@ -51,6 +54,7 @@
             console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
           } else {
             // codeReview(Anurag): I am passing the error which we get directly from nodemailer. Should change this to be more structured.
+            // feat():
             callback(error);
             console.log(error);
           }
